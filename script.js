37b97715330b79b0eecda874efe38dbe082b80bd
Peter Yang <peter@reviewerly.io>
2020-06-08 23:51:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// Store DOM elements for later use
+const cards = document.querySelectorAll('.card');
+const images = document.querySelectorAll('img');
+const restoreButton = document.querySelector('.btn-success');
+const mediaQueryList = window.matchMedia('(min-width: 800px)');
+const flexContainer = document.querySelector('.p-2');
+const greyBtn = document.querySelector('#greyscale');
+const blurBtn = document.querySelector('#blur');
+
+// Add event listeners to each card using loop to hide upon click
+for (let card of cards) {
+	card.addEventListener('click', function () {
+		this.style.display = 'none';
+	});
+}
+
+// Set onclick property of restoreButton to anonymous function
+restoreButton.onclick = function () {
+	// First loop makes cards reappear on screen
+	for (let card of cards) {
+		card.style.display = 'block';
+	}
+	// Second loop removes all classes from image nodes and then adds the "card-img-top" Bootstrap default class to each
+	for (let image of images) {
+		image.className = '';
+		image.classList.add('card-img-top');
+	}
+};
+
+// Add listener to mediaQueryList with corresponding callback function
+mediaQueryList.addListener(function (mql) {
+	// If screen resolution is less than 800px, add mobile class and remove desktop class
+	if (!mql.matches) {
+		flexContainer.classList.add('d-sm-flex');
+		flexContainer.classList.remove('d-flex');
+		// If screen resolution is at least 800px, add desktop class and remove mobile class
+	} else {
+		flexContainer.classList.add('d-flex');
+		flexContainer.classList.remove('d-sm-flex');
+		// Loop through cards and add 5px margins all around
+		for (let card of cards) {
+			card.style.margin = '5px';
+		}
+	}
+});
+
+// Set onclick property of greyBtn to anonymous function
+greyBtn.onclick = function () {
+	// Loop through images and ensure proper filter classes are active
+	for (let image of images) {
+		if (image.classList.contains('blur') && !image.classList.contains('greyscale'))
+			image.classList.toggle('blur-greyscale');
+		else if (image.classList.contains('blur-greyscale')) {
+			image.classList.toggle('blur-greyscale');
+			image.classList.toggle('greyscale');
+			image.classList.toggle('blur');
+		} else {
+			image.classList.toggle('greyscale');
+		}
+	}
+};
+
+// Set onclick property of blurBtn to anonymous function
+blurBtn.onclick = function () {
+	// Loop through images and ensure proper filter classes are active
+	for (let image of images) {
+		if (image.classList.contains('greyscale')) image.classList.toggle('blur-greyscale');
+		else if (image.classList.contains('blur-greyscale')) {
+			image.classList.toggle('blur-greyscale');
+			image.classList.toggle('greyscale');
+			image.classList.toggle('blur');
+		} else {
+			image.classList.toggle('blur');
+		}
+	}
+};
